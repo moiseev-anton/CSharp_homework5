7a92b1466aaf6fa18c4838efcde0b60f7afc975a
anton moiseev <moucee13@gmail.com>
2023-02-12 12:45:33 +0300

    Сделал задание 1

--- Task01(34)/Program.cs
+++ Task01(34)/Program.cs
@@ -3,25 +3,29 @@
 // [345, 897, 568, 234] -> 2
 
 Console.Clear();
-int[] array = GetArray();                        // Задаём массив
+int[] array = GetArray(20, 100, 1000);                        // Задаём массив
 Console.Write($"{String.Join(", ", array)}");    // Выводим массив в консоль
 Console.WriteLine();
 Console.Write($"Количество четных чисел = {PositivCount(array)}"); // Выводим результат
 
 
-int[] GetArray()                        // Метод задания массива случайных чисел.
+int[] GetArray(int size, int minVal, int maxVal)       // Метод задания массива случайных чисел.
 {
-    int[] arr = new int[20];                // Создаем пустой массив на 20 элементов
-    for (int i = 0; i < 20; i++)            // Циклом for перебираем все элементы массива
-        arr[i] = new Random().Next(100, 1000); // Присваиваем элементу массива рандомное число в интервале [100:1000)
-    return arr;                             // Возвращаем массив
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(minVal, maxVal);
+
+    return arr;
 }
 
 int PositivCount(int[] arr)         // Метод подсчета чётных чисел массива
 {
     int count = 0;
-    foreach (int i in arr)          // Перебираем массив. Если четное -> счётчик + 1.
-        if (i % 2 == 0)             
-            count++;               
+
+    foreach (int i in arr)          // Перебираем массив. Если элемент четный (без остатка делится на 2) -> счётчик + 1.
+        if (i % 2 == 0)
+            count++;
+            
     return count;
 }
